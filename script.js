52addea4be56eaccd9479a5ef7b9033685bda43a
oomo3069 <oomo1212312121@gmail.com>
2026-03-14 17:56:49 +0700

    about me

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const scrollBtn = document.querySelector('.scroll-down');
-  const portfolioSection = document.querySelector('.portfolio-section');
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollBtn = document.querySelector(".scroll-down");
+  const portfolioSection = document.querySelector(".portfolio-section");
   if (scrollBtn && portfolioSection) {
-    scrollBtn.addEventListener('click', function() {
-      portfolioSection.scrollIntoView({ behavior: 'smooth' });
+    scrollBtn.addEventListener("click", function () {
+      portfolioSection.scrollIntoView({ behavior: "smooth" });
     });
   }
 
-    if (!localStorage.getItem('portfolioAlertShown')) {
-    alert("Hello, welcome to my portfolio!🤓,This web have not responsive for mobile yet,💻 for experience you can use desktop or laptop to view this web,Thank you for your visit.👍");
-    localStorage.setItem('portfolioAlertShown', 'true');
+  if (!localStorage.getItem("portfolioAlertShown")) {
+    alert(
+      "Hello, welcome to my portfolio!🤓,This web have not responsive for mobile yet,💻 for experience you can use desktop or laptop to view this web,Thank you for your visit.👍",
+    );
+    localStorage.setItem("portfolioAlertShown", "true");
   }
-}); 
+});
